379b66f2bc96bc413c230feccf3c45e1dd01ff2a
Apprentice <apprentice@dbc06.local>
2013-04-26 15:39:10 -0700

    User can create account

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -3,6 +3,13 @@ get '/user/signup' do
   erb :user_signup
 end
 
+post '/user/signup' do 
+  @user = User.create(params)
+  session[:id] = @user.id
+  redirect "/user/#{@user.id}"
+end
+
+
 
 get '/user/login' do 
 
