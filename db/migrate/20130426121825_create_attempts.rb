fa375467cbb88ab64fbc62f5833928407a1c19f4
DBC student <apprentice@dbc05.local>
2013-04-26 16:29:38 -0700

    added migration tables

--- db/migrate/20130426121825_create_attempts.rb
+++ db/migrate/20130426121825_create_attempts.rb
@@ -1,4 +1,7 @@
 class CreateAttempts < ActiveRecord::Migration
   def change
+    create_table :attempts do |t|
+      t.references :game, :card
+      t.boolean :status
   end
 end
--- db/migrate/20130426121851_create_games.rb
+++ db/migrate/20130426121851_create_games.rb
@@ -1,4 +1,8 @@
 class CreateGames < ActiveRecord::Migration
   def change
+    create_table :games do |t|
+      t.references :users, :decks
+      t.boolean :status
+    end
   end
 end
