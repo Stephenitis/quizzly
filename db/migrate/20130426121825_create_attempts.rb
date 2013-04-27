4fc96ed8ca13949365157122ea9fec50ba76813d
DBC student <apprentice@dbc05.local>
2013-04-26 21:00:24 -0700

    added index to attempt table

--- db/migrate/20130426121825_create_attempts.rb
+++ db/migrate/20130426121825_create_attempts.rb
@@ -1,8 +1,10 @@
 class CreateAttempts < ActiveRecord::Migration
   def change
     create_table :attempts do |t|
-      t.references :game, :card
+      t.references :game
+      t.references :card
       t.boolean :status
     end
+    add_index :attempts, [:card_id, :game_id]
   end
 end
