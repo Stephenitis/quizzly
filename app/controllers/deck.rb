1a81a3a094a2fb3124b25237644de919714718e8
Stephenitis <nguyenhstephen@gmail.com>
2013-04-26 17:45:16 -0700

    user can create a new deck

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -4,9 +4,30 @@ get '/decks' do
   erb :decks_index
 end
 
+get '/deck/new' do
+
+  erb :deck_new
+end
+
+
+post '/deck/new' do
+  @deck = Deck.create(:name => params[:name])
+  if @deck 
+    redirect "deck/#{@deck.id}"
+  else
+    @errors = "you need to name your deck"
+    erb :deck_new
+  end
+end
+
+
+
 
 # show card for this deck id
 get '/deck/:id' do 
   @deck = Deck.find(params[:id])
   erb :deck
 end
+
+
+
