7f24616a3f0247ee10f6b563e0d76869115e6b50
DBC student <apprentice@dbc05.local>
2013-04-26 18:21:01 -0700

    user is able to answer true or false for a question

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -12,8 +12,16 @@ get '/game/' do
 end
 
 get '/game/id/:id' do
-  @game = Game.find(params[:id])
+  @game_id = params[:id]
+  @game = Game.find(@game_id)
+  @last_card_id = @game.attempts.last.card.id
   @deck_of_cards = @game.deck.cards
   erb :game_page
+end
+
+post '/game/attempt' do
+  att = Attempt.create(params[:game_play])
+  p att
+  redirect to '/game/id/2'
 end
 
