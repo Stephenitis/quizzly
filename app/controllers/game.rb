493daa80aae509a1002babdc77b0e9126002f775
Stephenitis <nguyenhstephen@gmail.com>
2013-04-27 17:09:21 -0700

    fixed navigation; changed button sizes; added description to decks in db; changed seed file

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -4,6 +4,7 @@ end
 
 post '/game/create' do
   @game = Game.create(params[:game_deck])
+  session[:deck_id] = @game.deck.id
   session[:cards_in_current_game] = @game.deck.cards.map { |card| card.id } 
   session[:current_game_id] = @game.id
   session[:current_card_position] = 0
@@ -15,10 +16,12 @@ get '/game/' do
 end
 
 get '/game/play' do
+
   unless session[:cards_in_current_game].length == session[:current_card_position]
     current_card_id = session[:cards_in_current_game][session[:current_card_position]]
     @current_card = Card.find(current_card_id)
     @current_game_id = session[:current_game_id]
+    @deck = Deck.find(session[:deck_id])
     erb :game_page
   else
     "congrats you are done"
@@ -31,3 +34,7 @@ post '/game/attempt' do
   redirect to '/game/play'
 end
 
+post '/game/quit' do
+  quit_game
+  redirect to '/game/play'
+end
--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -2,4 +2,8 @@ helpers do
   def list_all_decks 
     Deck.all
   end
+
+  def quit_game
+    session[:current_card_position] = session[:cards_in_current_game].length
+  end
 end
